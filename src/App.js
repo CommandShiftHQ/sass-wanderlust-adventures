0890bf3c38f4f6bc4d6379c19accceb35448bf7c
Catherine <catherinemcdonough3@gmail.com>
2023-04-12 13:37:30 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,159 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  HiOutlineCalendarDays as Calendar,
+  HiArrowRight as ArrowR,
+  HiOutlineTrophy as Trophy,
+  HiOutlineGlobeEuropeAfrica as Globe,
+} from 'react-icons/hi2';
+
+import { blogCardData, topSightsData } from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      {/* HEADER */}
+      <header>
+        <div>
+          <span>
+            Wanderlust <Globe /> Adventures
+          </span>
+        </div>
+        <nav>
+          <a>Destinations</a>
+          <a>Shop</a>
+          <a>Stories</a>
+        </nav>
       </header>
-    </div>
+      <main>
+        {/* HERO */}
+        <section>
+          <div></div>
+          <img
+            src='./images/hero.jpg'
+            alt='Scenic Canadian Landscape'
+          />
+        </section>
+        {/* OVERVIEW SECTION */}
+        <section>
+          <div>
+            <h1>
+              Canada<span>North America</span>
+            </h1>
+            <p>
+              Canada is more than its hulking-mountain, craggy-coast good looks:
+              it also cooks extraordinary meals, rocks cool culture, and unfurls
+              wild, moose-spotting road trips.
+            </p>
+          </div>
+          <div>
+            <h2>
+              Start planning your trip
+            </h2>
+            <div>
+              <div>
+                <div>
+                  <Calendar />
+                </div>
+                <div>
+                  <h5>Best Time to Visit</h5>
+                  <ArrowR />
+                </div>
+              </div>
+              <div>
+                <div>
+                  <Trophy />
+                </div>
+                <div>
+                  <h5>Best Places to Visit</h5>
+                  <ArrowR />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <hr />
+        {/* FEATURED CARDS */}
+        <section >
+          <h2>01 / Top Sights & Places</h2>
+          <h3>
+            Unmissable <span>sights</span> for
+            your itinerary
+          </h3>
+          <div>
+            {topSightsData.map((topSight) => (
+              <div>
+                <img src={topSight.image.src} alt={topSight.image.alt} />
+                <div >
+                  <h5>{topSight.title}</h5>
+                  <p>{topSight.subtitle}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div>
+            <a href=''>
+              Discover More Places
+            </a>
+          </div>
+        </section>
+        {/* CTA BANNER */}
+        <section>
+          <h2>02 / Guidebooks</h2>
+          <div>
+            <h3>
+              Purchase our award-winning guidebooks
+            </h3>
+            <p>
+              Get to the heart of Canada with one of our in-depth, award-winning
+              guidebooks, covering maps, itineraries, and expert guidance.
+            </p>
+            <a href=''>
+              Shop Our Guidebooks
+            </a>
+          </div>
+        </section>
+        {/* FEATURED ARTICLES */}
+        <section>
+          <h2>03 / Articles</h2>
+          <h3>
+            Latest <span>stories</span> from
+            Canada
+          </h3>
+          <div>
+            {blogCardData.map((blogCard) => (
+              <div>
+                <div>
+                  <img src={blogCard.image.src} alt={blogCard.image.alt} />
+                </div>
+                <div>
+                  <p>{blogCard.tag}</p>
+                  <h5>{blogCard.title}</h5>
+                  <p>{blogCard.date}</p>
+                  <p>{blogCard.overview}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div>
+            <a href=''>
+              Discover More Places
+            </a>
+          </div>
+        </section>
+      </main>
+      {/* FOOTER */}
+      <footer>
+        <div>
+          <span>
+            Wanderlust <Globe /> Adventures
+          </span>
+        </div>
+        <nav>
+          <a>About Us</a>
+          <a>Privacy Policy</a>
+          <a>Contact Us</a>
+        </nav>
+      </footer>
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,67 @@
+export const topSightsData = [
+  {
+    title: 'Jasper National Park',
+    subtitle: 'in Jasper Town & Around',
+    image: {
+      src: './images/jasper.jpg',
+      alt: 'image alt tag',
+    },
+  },
+  {
+    title: 'Banff National Park',
+    subtitle: 'in Alberta',
+    image: {
+      src: './images/banff.jpg',
+      alt: 'image alt tag',
+    },
+  },
+  {
+    title: 'Stanley Park',
+    subtitle: 'in Vancouver',
+    image: {
+      src: './images/stanley-park.jpg',
+      alt: 'image alt tag',
+    },
+  },
+  {
+    title: 'Lake Louise',
+    subtitle: 'in Lake Louise',
+    image: {
+      src: './images/lake-louise.jpg',
+      alt: 'image alt tag',
+    },
+  },
+];
+
+export const blogCardData = [
+  {
+    image: {
+      src: './images/camping.jpg',
+      alt: 'image alt text',
+    },
+    tag: 'Camping',
+    title: `Camping in Canada's national parks? Reservations open soon - and you'll have to be quick`,
+    date: `Feb 21, 2023 • 4 Min read`,
+    overview: `A new online campsite reservation system for some of Canada's most popular destinations is designed to make it simpler to secure your ultimate summer plans.`,
+  },
+  {
+    image: {
+      src: './images/coffee-shop.jpg',
+      alt: 'image alt text',
+    },
+    tag: 'Shopping',
+    title: `These six small businesses serve their community and travelers alike`,
+    date: `Feb 2, 2023 • 10 Min read`,
+    overview: `We asked our readers to nominate their favorite small businesses. Now we're highlighting some of our favorites.`,
+  },
+  {
+    image: {
+      src: './images/airport.jpg',
+      alt: 'image alt text',
+    },
+    tag: 'Tips & Advice',
+    title: `Do I need a visa to visit Canada?`,
+    date: `Jan 21, 2023 • 4 Min read`,
+    overview: `Many travelers to Canada need a visa or Electronic Travel Authorization to visit. Here's a quick guide to the visa requirements for Canada.`,
+  },
+];
